8fdb18d8cb79f6f5efdfa4a9510e47815b7e2f76
flp <>
2022-06-29 17:23:43 -0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
+
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+}
