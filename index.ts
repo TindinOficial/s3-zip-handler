10039a8fbf4acfc51fdc600af2432b7bcab12529
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2022-07-01 13:46:15 -0300

    feat: add decompressLocal and decompressToKeyFolderS3 functions

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import path from "path"
+import os from "os"
 import fs from "fs"
 import S3 from "aws-sdk/clients/s3"
 import { s3 } from "./src/s3"
@@ -22,4 +24,38 @@ const getDirectoryToExtract = (pathToExtract?: string) => {
     return tmpPath
   }
   return pathToExtract
+}
+
+interface IS3ZipFileParams {
+  s3Client: S3
+  bucket: string
+  key: string
+}
+
+const decompressLocal = async (zipFile: IS3ZipFileParams, pathToExtract?: string) => {
+  const { s3Client, bucket, key } = zipFile
+  const dirToExtract = getDirectoryToExtract(pathToExtract)
+  const decompressor = s3.decompression({ dir: dirToExtract })
+
+  const { s3file, zipName } = await s3.getFile({ bucket, key, s3: s3Client })
+
+  await decompressor(s3file, zipName)
+}
+
+const decompressToKeyFolderS3 = async (zipFile: IS3ZipFileParams) => {
+  const { s3Client, bucket, key } = zipFile
+
+  const dirToExtract = getDirectoryToExtract()
+  const dirToUpload = path.dirname(key)
+  const uploader = createUploader({ s3: s3Client, bucket, path: dirToUpload })
+  const decompressor = s3.decompression({ dir: dirToExtract, uploader })
+
+  const { s3file, zipName } = await s3.getFile({ bucket, key, s3: s3Client })
+
+  await decompressor(s3file, zipName)
+}
+
+export {
+  decompressLocal,
+  decompressToKeyFolderS3
 }
