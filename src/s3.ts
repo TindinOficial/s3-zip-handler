70f9786f9ad2401f59e2d039720f86e0836f9dad
felipeLeao18 <flpleao1807@gmail.com>
2022-07-05 09:25:28 -0300

    refactor: rename decompression to createDecompressor

--- src/s3.ts
+++ src/s3.ts
@@ -23,10 +23,10 @@ const getFile = async ({ bucket, key, s3 }: { bucket: string, key: string, s3: S
   return { s3dirPath: fileDirPath, s3file: s3File, zipName: file }
 }
 
-const decompression = (decompressionSettings: { dir: string, uploader?: (pathDir: string, zipName: string) => Promise<void> }) =>
+const createDecompressor = (decompressionSettings: { dir: string, uploader?: (pathDir: string, zipName: string) => Promise<string> }) =>
   async (file: CentralDirectory, fileZip: string) => {
     const { dir, uploader } = decompressionSettings
-    await file.extract({ path: dir })
+    await file?.extract({ path: dir })
 
     const tmpFolder = path.join(dir, fileZip)
     fs.accessSync(tmpFolder)
@@ -39,9 +39,33 @@ const decompression = (decompressionSettings: { dir: string, uploader?: (pathDir
     return { localPath: tmpFolder }
   }
 
+const createUploader = (s3Settings: { s3: S3, bucket: string, path?: string }) => async (pathDir: string, zipName: string) => {
+  const { s3, bucket, path: pathReceived } = s3Settings
+  const pathToExtract = pathReceived ?? ''
+  const dir = fs.opendirSync(pathDir)
+
+  console.log({ dir })
+  for await (const file of dir) {
+    console.log('entrou')
+    const fileCreated = path.join(pathDir, file.name)
+    const read = fs.createReadStream(fileCreated)
+
+    const uploadPath = path.join(pathToExtract, zipName, file.name)
+    console.log('entrou', { uploadPath, read, fileCreated, file })
+    console.log(`${pathToExtract}/${zipName}/${file.name}`)
+    await s3.upload({ Key: uploadPath, Bucket: bucket, Body: read }).promise()
+  }
+  console.log('passou')
+
+  const createdS3Path = path.join(pathToExtract, zipName)
+  console.log(createdS3Path)
+  return createdS3Path
+}
+
 const s3 = {
   getFile,
-  decompression
+  createDecompressor,
+  createUploader
 }
 
 export {
