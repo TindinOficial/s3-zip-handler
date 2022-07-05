6d4d9cd2a293b91f9bbb5f5737115dc3a7ef14ef
felipeLeao18 <flpleao1807@gmail.com>
2022-07-05 13:04:01 -0300

    test: ensure folders provided from zip file are being kept the same from origin when unzipper

--- src/tests/createUploader.test.ts
+++ src/tests/createUploader.test.ts
@@ -3,6 +3,7 @@ import AWS from 'aws-sdk'
 import fs, { Dir } from 'fs'
 import path from 'path'
 import S3 from 'aws-sdk/clients/s3'
+import os from 'node:os'
 
 jest.mock('aws-sdk')
 const AWS_MOCK = jest.mocked(AWS, true)
@@ -18,7 +19,9 @@ describe('unit: create uploader', () => {
     }) as unknown as S3)
     const mockedS3 = new AWS_MOCK.S3() as any
 
-    jest.spyOn(fs, 'opendirSync').mockImplementationOnce(() => [{ name: 'any_file.html' }, { name: 'any_file.css' }] as unknown as Dir)
+    jest.spyOn(fs, 'opendirSync').mockImplementationOnce(() =>
+      [{ name: 'any_file.html', isDirectory: () => false },
+        { name: 'any_file.css', isDirectory: () => false }] as unknown as Dir)
 
     jest.spyOn(path, 'join').mockImplementationOnce(() => 'any_valid_path/any_valid_file')
     jest.spyOn(fs, 'createReadStream').mockImplementationOnce(() => {
@@ -35,7 +38,9 @@ describe('unit: create uploader', () => {
   })
 
   it('should reject when s3 upload fails', async () => {
-    jest.spyOn(fs, 'opendirSync').mockImplementationOnce(() => [{ name: 'any_file.html' }, { name: 'any_file.css' }] as unknown as Dir)
+    jest.spyOn(fs, 'opendirSync').mockImplementationOnce(() =>
+      [{ name: 'any_file.html', isDirectory: () => false },
+        { name: 'any_file.css', isDirectory: () => false }] as unknown as Dir)
     jest.spyOn(path, 'join').mockImplementationOnce(() => 'any_valid_path/any_valid_file')
     jest.spyOn(fs, 'createReadStream').mockImplementationOnce(() => ({ insideReadStream: true }) as unknown as fs.ReadStream)
 
@@ -55,6 +60,40 @@ describe('unit: create uploader', () => {
     }
   })
 
+  it('should resolve when all data provided is correct and validate recursive folder provided from zip file', async () => {
+    const UPLOAD_BUFFER: any[] = []
+    AWS_MOCK.S3.mockImplementationOnce(() => ({
+      upload: jest.fn(({ Body, Bucket, Key }) => {
+        UPLOAD_BUFFER.push({ Body, Bucket, Key })
+        return { promise: jest.fn() }
+      })
+
+    }) as unknown as S3)
+    const mockedS3 = new AWS_MOCK.S3() as any
+
+    const tmpFolderProps = path.join(os.tmpdir(), 'valid-folder-')
+    const tmpFolderPath = fs.mkdtempSync(tmpFolderProps)
+
+    fs.mkdirSync(path.join(tmpFolderPath, 'folder_to_be_unzipped'))
+
+    fs.writeFileSync(path.join(tmpFolderPath, 'folder_to_be_unzipped', 'my_file.html'), '')
+
+    jest.spyOn(fs, 'createReadStream').mockImplementation((path) => ({ path }) as unknown as fs.ReadStream)
+
+    const key = 'any_folder'
+    const uploader = await s3.createUploader({ s3: mockedS3, bucket: 'any_bucket', key })
+
+    const zipName = 'any_zip'
+    const createdS3Path = await uploader(tmpFolderPath, zipName)
+
+    expect(createdS3Path).toBe(path.join(key, zipName))
+    expect(UPLOAD_BUFFER.length).toBe(1)
+    const [buffer] = UPLOAD_BUFFER
+    expect(buffer.Body.path).toBe(path.join(tmpFolderPath, 'folder_to_be_unzipped', 'my_file.html'))
+
+    fs.rmdirSync(tmpFolderPath, { recursive: true })
+  })
+
   it('should resolve when all data provided is correct', async () => {
     AWS_MOCK.S3.mockImplementationOnce(() => ({
       upload: jest.fn().mockReturnThis(),
@@ -62,7 +101,9 @@ describe('unit: create uploader', () => {
     }) as unknown as S3)
     const mockedS3 = new AWS_MOCK.S3() as any
 
-    jest.spyOn(fs, 'opendirSync').mockImplementationOnce(() => [{ name: 'any_file.html' }, { name: 'any_file.css' }] as unknown as Dir)
+    jest.spyOn(fs, 'opendirSync').mockImplementationOnce(() =>
+      [{ name: 'any_file.html', isDirectory: () => false },
+        { name: 'any_file.css', isDirectory: () => false }] as unknown as Dir)
 
     jest.spyOn(path, 'join').mockImplementationOnce(() => 'any_valid_path/any_valid_file')
     jest.spyOn(fs, 'createReadStream').mockImplementation(() => ({ insideReadStream: true }) as unknown as fs.ReadStream)
